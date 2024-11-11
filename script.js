e37aa5a31d4ef7fbb2c29d4c128cdaa9d9060600
Tri Phan <99871813+triphanngoc@users.noreply.github.com>
2024-11-11 11:58:51 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// You can add JavaScript here to enhance your page, like:
+// - Adding interactive elements
+// - Handling user input
+// - Making AJAX requests
+
+// For example, a simple alert:
+document.querySelector('button').addEventListener('click', () => {
+    alert('Button clicked!');
+});
